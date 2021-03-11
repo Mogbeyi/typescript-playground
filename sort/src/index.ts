034c32f5b6cd82ce7541bd93a996e89306a369d6
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2021-03-11 14:42:38 +0100

    Create sortable interface

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0, 100, -100])
+const numbersCollection = new NumbersCollection([1000, 3, -5, 0, 100, -100])
 const sorter = new Sorter(numbersCollection)
 sorter.sort()
 console.log(sorter.collection)
